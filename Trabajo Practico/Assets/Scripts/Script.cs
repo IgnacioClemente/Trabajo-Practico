cf053b556e3dec4c1631bb3e1e5576c61c56a564
hansow <nachoagus@live.com.ar>
2016-09-20 12:01:39 -0300

    yep
    Signed-off-by: hansow <nachoagus@live.com.ar>

---
+++ Trabajo Practico/Assets/Scripts/Script.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Script : MonoBehaviour
+{
+    public Animator AnimationController;
+
+    // Use this for initialization
+    void Start()
+    {
+        AnimationController = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            AnimationController.Play("playeridle");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            AnimationController.Play("Run");
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            AnimationController.Play("Jump");
+        }
+        
+    }
+}
